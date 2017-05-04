4c81780f82f54689d0bfd637fb2628ceab4ffc62
Cpzty <Cristianpzty@gmail.com>
2017-05-04 11:33:55 -0600

    Deshaciendo la detonacion masiva

--- app/src/main/java/com/example/cristian/healthapp/SpeechtoText.java
+++ app/src/main/java/com/example/cristian/healthapp/SpeechtoText.java
@@ -2,41 +2,34 @@ package com.example.cristian.healthapp;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.ibm.watson.developer_cloud.natural_language_understanding.v1.NaturalLanguageUnderstanding;
-import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.AnalysisResults;
-import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.AnalyzeOptions;
-import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.EntitiesOptions;
-import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.Features;
 
 import java.util.ArrayList;
 import java.util.Locale;
 
 //import com.ibm.watson.developer_cloud.dialog.v1.model.Conversation;
 
-public class SpeechtoText extends AppCompatActivity {
-
-    ArrayList<String> result;
+public class SpeechtoText extends AppCompatActivity  {
 
 
-    private TextView resultTEXT;
+
+    private  TextView resultTEXT;
     private TextView watsonResp;
     private String text;
-    private Button btnResponse;
     // private Conversation service;
     NaturalLanguageUnderstanding service = new NaturalLanguageUnderstanding(
             NaturalLanguageUnderstanding.VERSION_DATE_2017_02_27,
             "e431d06f-091e-45df-b7b0-eb80ade5de83",
             "XZ4lshXtyhuR"
     );
+
+
+
 
 
     @Override
@@ -48,32 +41,17 @@ public class SpeechtoText extends AppCompatActivity {
 //        service.setClientId("username", "password");
 //        service.set(QuestionAndAnswerDataset.TRAVEL);
         resultTEXT =  (TextView)findViewById(R.id.TVresult);
-        btnResponse = (Button)findViewById(R.id.btnAnalizar);
     }
 
-    private class Llamada extends AsyncTask<Void, Void, String>{
-        @Override
-        protected String doInBackground(Void... params) {
-            NaturalLanguageUnderstanding service = new NaturalLanguageUnderstanding(
-                    NaturalLanguageUnderstanding.VERSION_DATE_2017_02_27,
-                    "e431d06f-091e-45df-b7b0-eb80ade5de83",
-                    "XZ4lshXtyhuR"
-            );
-            service.setEndPoint("https://gateway.watsonplatform.net/natural-language-undesrtanding/api");
-            EntitiesOptions entities = new EntitiesOptions.Builder().sentiment(true).limit(1).model("20:f080ec47-b6e0-4cbe-96e4-fd1d98e11e54").build();
-            Features features = new Features.Builder().entities(entities).build();
-            AnalyzeOptions parameters = new AnalyzeOptions.Builder()
-                    .text(result.get(0))
-                    .features(features)
-                    .build();
-            AnalysisResults results = service.analyze(parameters).execute();
-            return results.toString();
+    public void onButtonClick(View v){
+
+        if(v.getId() == R.id.imageButton){
+
+
+            promptSpeechInput();
+
         }
 
-        @Override
-        protected void onPostExecute(String st){
-            resultTEXT.setText(st);
-        }
     }
 
     public void promptSpeechInput(){
@@ -90,17 +68,6 @@ public class SpeechtoText extends AppCompatActivity {
         }
     }
 
-    public void onButtonClick(View v){
-
-        if(v.getId() == R.id.imageButton){
-
-
-            promptSpeechInput();
-
-        }
-
-    }
-
     public void onActivityResult(int request_code, int result_code, Intent i){
         super.onActivityResult(request_code,result_code,i);
 
@@ -108,11 +75,14 @@ public class SpeechtoText extends AppCompatActivity {
             case 100: if(result_code== RESULT_OK && i != null){
                 ArrayList<String>result = i.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                 resultTEXT.setText(result.get(0));
-                new Llamada().execute();
 
             }
                 break;
         }
     }
 
+
+
+
 }
+
