a24a5ce1e58da16768e30298cff75ce2e704a912
Cpzty <Cristianpzty@gmail.com>
2017-05-07 19:07:32 -0600

    Deshaciendo la detonacion masiva todavia, ya esta solo es de hacer los parametros

--- app/src/main/java/com/example/cristian/healthapp/SpeechtoText.java
+++ app/src/main/java/com/example/cristian/healthapp/SpeechtoText.java
@@ -2,12 +2,19 @@ package com.example.cristian.healthapp;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.AnalysisResults;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.AnalyzeOptions;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.EntitiesOptions;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.Features;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -16,11 +23,33 @@ import java.util.Locale;
 
 public class SpeechtoText extends AppCompatActivity  {
 
+    public class NL extends AsyncTask<Void,Void,String> {
+        @Override
+        protected String doInBackground(Void... params) {
+            EntitiesOptions entities = new EntitiesOptions.Builder().sentiment(true).limit(1).build();
+            Features features = new Features.Builder().entities(entities).build();
+            AnalyzeOptions parameters = new AnalyzeOptions.Builder()
+                    .text(resultTEXT.toString())
+                    .features(features)
+                    .build();
+            AnalysisResults results = service.analyze(parameters).execute();
+            return results.toString();
+        }
 
 
-    private  TextView resultTEXT;
+
+        @Override
+        protected void onPostExecute(String s){resultTEXT.setText(s);}
+    }
+
+
+
+
+
+    public   TextView resultTEXT;
     private TextView watsonResp;
     private String text;
+    private Button btnResponse;
     // private Conversation service;
     NaturalLanguageUnderstanding service = new NaturalLanguageUnderstanding(
             NaturalLanguageUnderstanding.VERSION_DATE_2017_02_27,
@@ -41,6 +70,7 @@ public class SpeechtoText extends AppCompatActivity  {
 //        service.setClientId("username", "password");
 //        service.set(QuestionAndAnswerDataset.TRAVEL);
         resultTEXT =  (TextView)findViewById(R.id.TVresult);
+        btnResponse = (Button)findViewById(R.id.btnAnalizar);
     }
 
     public void onButtonClick(View v){
@@ -51,6 +81,21 @@ public class SpeechtoText extends AppCompatActivity  {
             promptSpeechInput();
 
         }
+
+        if(v.getId()==R.id.btnAnalyze){
+            resultTEXT.setText("Alahu akbar");
+
+            service.analyze(parameters).execute();
+
+
+
+
+
+
+
+        }
+
+
 
     }
 
@@ -84,5 +129,7 @@ public class SpeechtoText extends AppCompatActivity  {
 
 
 
-}
 
+
+
+}
